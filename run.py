617330fd8e4c620a263e9015cc6947491771f9be
ChiYoSeop <gowldytjq98@hanyang.ac.kr>
2022-05-24 14:32:55 +0900

    05/24

--- run.py
+++ run.py
@@ -3,17 +3,17 @@ import os
 
 # from src import filter
 # import filter
-from src import filter, parsing, graph, fitting
+from src import filter, parsing, graph, fitting, csv
 
 
 '''찾고 싶은 파일의 Lot_id를 입력하세요.'''
 Lot_id = ['HY202103']
 
 '''찾고 싶은 파일의 Wafer_id를 입력하세요.'''
-Wafer_id = ['D08']
+Wafer_id = []
 
 '''찾고 싶은 파일의 행렬을 입력하세요. ex) [0,0]'''
-xy_coord = ['(0,2)']
+xy_coord = ["(2,-1)"]
 
 '''찾고 싶은 파일의 maskset을 입력하세요.'''
 Mask_set = ['LION1']
@@ -22,14 +22,15 @@ Mask_set = ['LION1']
 device_name =[]
 
 '''그래프를 저장하고 싶다면 True, 저장하고 싶지 않다면 False를 입력하세요.'''
-Opt_savefig = True
+Opt_savefig = False
 
 '''그래프를 보고 싶다면 True, 보고 싶지 않다면 False를 입력하세요.'''
 Opt_showfig = False
 
 
 filter.cmp(Lot_id,Wafer_id,xy_coord,Mask_set,device_name)
-# print(str(filter.dlst[0]))
+# print((filter.dlst))
+
 for i in range(len(filter.dlst)):
     a = parsing.v(filter.dlst[i])
     b = parsing.i(filter.dlst[i])
@@ -39,7 +40,7 @@ for i in range(len(filter.dlst)):
     # s = str(filter.dlst[i])
     f = graph.figname(str(filter.dlst[i]))
     graph.grph(a,b,c,d,e,Opt_showfig,Opt_savefig,f)
-
+    csv.csv(filter.dlst[i],csv.csvname(str(filter.dlst[i])))
 # print(a.pas())
 # print(parsing.i)
 # print(graph.v(filter.dlst))
