4e7a89cbdc77d15812fd6c1ee70a5794e247f159
hiry0 <realhr0805@hanyang.ac.kr>
2022-05-26 10:58:53 +0900

    filter 수정

--- src/Try_com.py
+++
@@ -1,9 +0,0 @@
-import xml.etree.ElementTree as ET
-import os
-from pathlib import Path
-
-
-b =Path("C:\\Users\\gunwo\\PycharmProjects\\A3TW\\dat\\D07\\20190715_190855").resolve()
-
-for ret in b.glob("*(0,0)*LMZC*") :
-     print(ret)
