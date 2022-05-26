e8f699279fa5fbb162984dfdb5f98ed328c6e01b
hiry0 <realhr0805@hanyang.ac.kr>
2022-05-26 10:52:36 +0900

    hi

---
+++ src/get_start.py
@@ -0,0 +1,30 @@
+import warnings
+def run2u(a,b,c,d,f,g):
+    print('안녕하세요')
+    for i in range(len(a.dlst)):
+        warnings.filterwarnings('ignore')
+        aa = b.v(a.dlst[i])
+        bb = b.i(a.dlst[i])
+        cc = b.wvl(a.dlst[i])
+        dd = b.itst(a.dlst[i])
+        ee = b.lgds(a.dlst[i])
+        ff = c.figname(str(a.dlst[i]))
+        print(a.dlst[i])
+        t = c.grp(aa, bb, cc, dd, ee, f, g, ff)
+        t.grph()
+        d.csv(a.dlst[i], d.csvname(str(a.dlst[i])), t, b)
+    print('분석이 완료되었습니다. 이용해 주셔서 감사합니다.')
+
+# print(str(filter.dlst[0]))
+# for i in range(len(filter.dlst)):
+#     warnings.filterwarnings('ignore')
+#     a = parsing.v(filter.dlst[i])
+#     b = parsing.i(filter.dlst[i])
+#     c = parsing.wvl(filter.dlst[i])
+#     d = parsing.itst(filter.dlst[i])
+#     e = parsing.lgds(filter.dlst[i])
+#     f = graph.figname(str(filter.dlst[i]))
+#     print(filter.dlst[i])
+#     t = graph.grp(a,b,c,d,e,Opt_showfig,Opt_savefig,f)
+#     t.grph()
+#     cs = csv_maker.csv(filter.dlst[i], csv_maker.csvname(str(filter.dlst[i])), t, parsing)
